b79fc39ef5d171b0cccb8d1ba64a0e84e785fcef
Avenger7x <avenger7x13@gmail.com>
2014-03-15 00:58:44 +0200

    manifests

--- app/index.js
+++ app/index.js
@@ -6,16 +6,6 @@ var chalk = require('chalk');
 
 
 var ProjectGenerator = yeoman.generators.Base.extend({
-  init: function () {
-    this.pkg = require('../package.json');
-
-    /*this.on('end', function () {
-      if (!this.options['skip-install']) {
-        this.installDependencies();
-      }
-    });*/
-  },
-
   askFor: function () {
     var done = this.async();
 
@@ -26,14 +16,16 @@ var ProjectGenerator = yeoman.generators.Base.extend({
     this.log(chalk.magenta('You\'re using the fantastic Project generator.'));
 
     var prompts = [{
-      type: 'confirm',
-      name: 'someOption',
-      message: 'Would you like to enable this option?',
-      default: true
+      name: 'projectName',
+      message: 'project name'
+    }, {
+      name: 'description',
+      message: 'description'
     }];
 
     this.prompt(prompts, function (props) {
-      this.someOption = props.someOption;
+      this.projectName = props.projectName;
+      this.description = props.description;
 
       done();
     }.bind(this));
@@ -46,12 +38,12 @@ var ProjectGenerator = yeoman.generators.Base.extend({
     this.copy('gitattributes', '.gitattributes');
     this.copy('gitignore', '.gitignore');
 
-    // bower.json
-    // package.json
-    // component.json
+    this.copy('bower.json', 'bower.json');
+    this.copy('package.json', 'package.json');
+    this.copy('component.json', 'component.json');
 
-    // index.js
-    // readme.md
+    this.copy('index.js', 'index.js');
+    this.copy('readme.md', 'readme.md');
   }
 });
 
---
+++ app/templates/readme.md
@@ -0,0 +1,3 @@
+# <%= _.capitalize(projectName) %>
+
+<%= description %>
